9feaad62c613a5ceb9f303528106ad4937b09b8d
Manikonda,Sai Sri Dasarsh Rajarao <s528807@ch140030m.nwmissouri.edu>
2017-10-04 12:01:28 -0500

    Initial Commit

---
+++ GitDemo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GitDemo
+//
+//  Created by Manikonda,Sai Sri Dasarsh Rajarao on 10/4/17.
+//  Copyright © 2017 Manikonda,Sai Sri Dasarsh Rajarao. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
